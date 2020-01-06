6da48cd213be015ca71e28c41f20c7ddb816f0b6
maojui <maojui0427@gmail.com>
2020-01-06 16:27:02 +0800

    Merge djosix's padding oracle script.

--- cytro/sym/cbc/PaddingOracle.py
+++ cytro/sym/cbc/PaddingOracle.py
@@ -1,6 +1,8 @@
-import re
-import logging as log
-from cytro import *
+import logging
+import base64
+import urllib.parse
+from typing import Union, Callable
+from concurrent.futures import ThreadPoolExecutor
 
 def xor(cipher, _from, _to):
     print("XOR", cipher, _from , _to)
@@ -11,125 +13,134 @@ def xor(cipher, _from, _to):
             cipher = xor_string(cipher,target)
     return cipher
 
-class PaddingOracle:
+__all__ = [
+    'base64_encode', 'base64_decode',
+    'urlencode', 'urldecode',
+    'padding_oracle',
+    'remove_padding'
+]
+
+def _to_bytes(data: Union[str, bytes]):
+    if isinstance(data, str):
+        data = data.encode()
+    assert isinstance(data, bytes)
+    return data
+
+def _to_str(data):
+    if isinstance(data, bytes):
+        data = data.decode()
+    elif isinstance(data, str):
+        pass
+    else:
+        data = str(data)
+    return data
+
+def base64_decode(data: Union[str, bytes]) -> bytes:
+    data = _to_bytes(data)
+    return base64.b64decode(data)
+
+def base64_encode(data: Union[str, bytes]) -> str:
+    data = _to_bytes(data)
+    return base64.b64encode(data).decode()
+
+def urlencode(data: Union[str, bytes]) -> str:
+    data = _to_bytes(data)
+    return urllib.parse.quote(data)
+
+def urldecode(data: str) -> bytes:
+    data = _to_str(data)
+    return urllib.parse.unquote_plus(data)
+
+def remove_padding(data: Union[str, bytes]):
+    data = _to_bytes(data)
+    return data[:-data[-1]]
+
+def _dummy_oracle(cipher: bytes) -> bool:
+    raise NotImplementedError('You must implement the oracle function')
+
+
+def padding_oracle(cipher: bytes,
+                   block_size: int,
+                   oracle: Callable[[bytes], bool]=_dummy_oracle,
+                   num_threads: int=1,
+                   log_level: int=logging.INFO,
+                   null: bytes=b' ') -> bytes:
+    # Check the oracle function
+    assert callable(oracle), 'the oracle function should be callable'
+    assert oracle.__code__.co_argcount == 1, 'expect oracle function with only 1 argument'
+    assert len(cipher) % block_size == 0, 'cipher length should be multiple of block size'
+    
+    logger = logging.getLogger('padding_oracle')
+    logger.setLevel(log_level)
+    formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
+    # formatter = logging.Formatter('[%(asctime)s][%(name)s][%(levelname)s] %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # The plaintext bytes list to save the decrypted data
+    plaintext = [null] * (len(cipher) - block_size)
+
+    def _update_plaintext(i: int, c: bytes):
+        plaintext[i] = c
+        logger.info('plaintext: {}'.format(b''.join(plaintext)))
+
+    oracle_executor = ThreadPoolExecutor(max_workers=num_threads)
+    
+    def _block_decrypt_task(i, prev: bytes, block: bytes):
+        logger.debug('task={} prev={} block={}'.format(i, prev, block))
+        guess_list = list(prev)
         
-    def __init__(self, key_size=16):
-        # if key_size % 8 != 0:
-        #     raise ValueError("Incorrect key length: %d", key_size)
-        self.key_size = key_size
-
-    def oracle(self, payload, iv, previous_resp, **kwargs):
-        """
-        payload - hex encoded ciphertext to check
-        iv - hex encoded inicialization vector (most often: append it at beggining of payload)
-        previous_resp - value returned by previous call to oracle
-        Return tuple: (True/False, response)
-        True if padding is correct, Flase otherwise; Be carefull while sending original ciphertext
-        response will be send as previous_response to next call
-        """
-        raise NotImplementedError
-
-    def decrypt(self, ciphertext, iv=None, amount=0, is_correct=False, known_plaintext=None, **kwargs):
-        """
-        ciphertext - raw cipher (bytes)
-        iv - usually it's first block of ciphertext, but may be specified separate
-        amount - how much blocks decrypt (counting from last), default is all
-        is_correct - if True, then first decrypted byte will be treated as correct padding (speed up decryption)
-        known_plaintext - hex encoded, with padding, from end
-        kwargs - push forward to oracle function
-        Return raw plaintext
-        """
-        
-        if len(ciphertext)%self.key_size!=0:
-            raise ValueError("Incorrect ciphertext length: %d"%(len(ciphertext)))
-
-        #prepare blocks
-        blocks = chunk(ciphertext,self.key_size)
-
-        resp = None
-        if iv != None:
-            log.info("Set iv")
-            iv = iv
-            log.info("iv is : %s"%iv)
-            blocks.insert(0,iv)
-
-        if amount!=0:
-            amount=len(blocks)-amount-1
-
-        if amount<0 or amount>=len(blocks):
-            raise ValueError("Incorrect amount of blocks to decrypt: %d (have to be [0,%d)"%(amount,len(blocks)))
-        
-        #add known plaintext
-        plaintext = ''
-        position_known = 0
-        FIRST_MODIFY = False
-
-        if known_plaintext != None :
-            if len(known_plaintext) == 0:
-                raise ValueError("Knowing plaintext cannot be empty, must be None or bytes."%(amount,len(blocks)))
-            else :
-                FIRST_MODIFY = True
-                kp = known_plaintext
-                plaintext = known_plaintext
-                blocks_decoded = len(kp) // self.key_size
-                chars_decoded = len(kp) % self.key_size
-                if blocks_decoded>=len(blocks):
-                    raise RuntimeError("Too long known plaintext (%d blocks)" % blocks_decoded)
-                blocks = blocks[:len(blocks)-blocks_decoded]
-                position_known = chars_decoded
-
-
-        for count_block in range(len(blocks)-1, amount, -1):
-            # For every block 
-            payload_prefix = b''.join(blocks[:count_block-2])
-            if FIRST_MODIFY :
-                FIRST_MODIFY = False
-                print("origin",blocks[-2])
-                payload_modify = blocks[-2][:-position_known] + xor(blocks[-2][-position_known:],kp[:position_known],position_known+1)
-                
-            else :
-                payload_modify = blocks[count_block-1]
-
-            payload_decrypt = blocks[count_block]
+        for j in range(1, block_size + 1):
+            oracle_hits = []
+            oracle_futures = {}
             
-            position = self.key_size-1-position_known
-            position_known = 0
-
-            while position >= 0 :
-                # Every position in block 
-                for one in range(256):
-                    modified = payload_modify[:position] + byte(one) + payload_modify[position+1:]
-                    
-                    payload = b''.join([payload_prefix, modified, payload_decrypt])
-                    iv = payload[:self.key_size]
-                    payload = payload[self.key_size:]
-                    is_ok = False
-
-                    correct, resp = self.oracle(payload=payload, iv=iv, previous_resp=resp, **kwargs)
-                    if correct :
-                        """ oracle return True """
-                        padding = self.key_size-position
-                        
-                        decrypted_char = byte(payload_modify[position]^one^padding)
-                        if is_correct == True and count_block == len(blocks)-1 and position == self.key_size-1 and known_plaintext == None:
-                            """ Have original padding value, can skip some bytes """
-                            dc = ord(decrypted_char)
-                            log.info("Found padding value: %d", dc)
-                            if dc == 0 or dc > self.key_size:
-                                raise RuntimeError("Found bad padding value (given ciphertext may not be correct)")
-
-                            plaintext = decrypted_char * dc
-                            payload_modify = payload_modify[:-dc] + xor(payload_modify[-dc:],dc,dc+1)
-                            position = position-dc+1
-                        else:
-                            payload_modify = payload_modify[:position] + xor(byte(one) + payload_modify[position+1:], padding, padding+1)
-                            plaintext = decrypted_char+plaintext
-
-                        is_ok = True
-                        print('Plaintext: %s'% plaintext,end='\n')
-                        break
-                position -= 1
-                if is_ok == False:
-                    raise RuntimeError("Can't find corrent padding (oracle function return False 256 times)")
-        
-        return plaintext
+            for k in range(256):
+                if i == len(blocks) - 1 and j == 1 and k == prev[-j]:
+                    # skip the last padding byte if it is identical to the original cipher
+                    continue
+                
+                test_list = guess_list.copy()
+                test_list[-j] = k
+                oracle_futures[k] = oracle_executor.submit(oracle, bytes(test_list) + block)
+            
+            for k, future in oracle_futures.items():
+                if future.result():
+                    oracle_hits.append(k)
+            
+            logger.debug('oracles at block[{}][{}] -> {}'.format(i, block_size - j, oracle_hits))
+                
+            if len(oracle_hits) != 1:
+                logfmt = 'at block[{}][{}]: expect only one positive result, got {}. (skipped)'
+                logger.error(logfmt.format(i, block_size-j, len(oracle_hits)))
+                return
+            
+            guess_list[-j] = oracle_hits[0]
+            
+            p = guess_list[-j] ^ j ^ prev[-j]
+            _update_plaintext(i * block_size - j, bytes([p]))
+            
+            for n in range(j):
+                guess_list[-n-1] ^= j
+                guess_list[-n-1] ^= j + 1
+    
+    blocks = []
+    
+    for i in range(0, len(cipher), block_size):
+        j = i + block_size
+        blocks.append(cipher[i:j])
+    
+    logger.debug('blocks: {}'.format(blocks))
+    
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for i in reversed(range(1, len(blocks))):
+            prev = b''.join(blocks[:i])
+            block = b''.join(blocks[i:i+1])
+            futures.append(executor.submit(_block_decrypt_task, i, prev, block))
+        for future in futures:
+            future.result()
+    
+    oracle_executor.shutdown()
+    
+    return b''.join(plaintext)
